b4a1a77ee2d111618266f4f366e82452362db8bd
Aaron Porter <aaron.porter12@yahoo.com>
2024-04-29 09:35:48 -0500

    start over due to bullshit parsing that neither copilot or i could figure out

--- .vscode/reciever/reciver1.1/setup.ino
+++ .vscode/reciever/reciver1.1/setup.ino
@@ -2,24 +2,7 @@
 #include <LoRa.h>
 
 #define frequency 915E6
-
-volatile bool switch1State = false;
-volatile bool switch2State = false;
-
-#define RELAY1_PIN 1
-#define RELAY2_PIN 2
-#define LED1_PIN 3
-#define LED2_PIN 4
-
-void requestSwitchStates() {
-  String message = "requestSwitchStates";
-
-  LoRa.beginPacket();
-  LoRa.print(message);
-  LoRa.endPacket();
-
-  Serial.println("Sent: " + message); // Log the actual transmission
-}
+#define RELAY_PIN 1
 
 void setup() {
   Serial.begin(9600);
@@ -36,32 +19,8 @@ void setup() {
   LoRa.setSignalBandwidth(125E3);
   LoRa.setCodingRate4(5);
 
-  pinMode(RELAY1_PIN, OUTPUT);
-  pinMode(RELAY2_PIN, OUTPUT);
-  pinMode(LED1_PIN, OUTPUT);
-  pinMode(LED2_PIN, OUTPUT);
-
+  pinMode(RELAY_PIN, OUTPUT);
   Serial.println("LoRa init succeeded.");
-  // Request the switch states from the transmitter
-  delay(2000); // Give the LoRa module some time to stabilize
-  requestSwitchStates();
-}
-
-bool handleSwitchState(String received, volatile bool& switchState) {
-  received.trim();
-  int closedIndex = received.indexOf("closed");
-  int openIndex = received.indexOf("open");
-
-  // Only update the state if the received message contains switch state information
-  if (closedIndex != -1 || openIndex != -1) {
-    bool receivedState = closedIndex != -1;
-    if (receivedState != switchState) {
-      switchState = receivedState;
-      return true; // Return true to indicate that the switch state was updated
-    }
-  }
-
-  return false; // Return false to indicate that the switch state was not updated
 }
 
 void loop() {
@@ -73,21 +32,14 @@ void loop() {
       received += c;
     }
 
-    Serial.println("Received: " + received);
-    Serial.println("RSSI: " + String(LoRa.packetRssi()));
+    received.trim(); // Trim the received message
+    Serial.println("Received message: " + received);
 
-    if (received.startsWith("Switch 1")) {
-      if (handleSwitchState(received, switch1State)) {
-        // Update the outputs only if the switch state was updated
-        digitalWrite(RELAY1_PIN, switch1State ? HIGH : LOW);
-        digitalWrite(LED1_PIN, switch1State ? HIGH : LOW);
-      }
-    } else if (received.startsWith("Switch 2")) {
-      if (handleSwitchState(received, switch2State)) {
-        // Update the outputs only if the switch state was updated
-        digitalWrite(RELAY2_PIN, switch2State ? HIGH : LOW);
-        digitalWrite(LED2_PIN, switch2State ? HIGH : LOW);
-      }
+    if (received.startsWith("Switch 1 state changed: ")) {
+      String switchStateStr = received.substring(24); // Extract the switch state
+      switchStateStr.trim(); // Trim the switch state string
+      bool relayState = switchStateStr == "closed";
+      digitalWrite(RELAY_PIN, relayState ? HIGH : LOW);
     }
   }
 }
--- .vscode/transmitter/transmitter_1.1/setup.ino
+++ .vscode/transmitter/transmitter_1.1/setup.ino
@@ -2,14 +2,7 @@
 #include <LoRa.h>
 
 #define frequency 915E6
-
-#define SWITCH1_PIN 1
-#define SWITCH2_PIN 2
-#define SWITCH1_LED_PIN 3
-#define SWITCH2_LED_PIN 4
-
-volatile bool switch1State = false;
-volatile bool switch2State = false;
+#define SWITCH_PIN 1
 
 void setup() {
   Serial.begin(9600);
@@ -26,53 +19,18 @@ void setup() {
   LoRa.setSignalBandwidth(125E3);
   LoRa.setCodingRate4(5);
 
-  pinMode(SWITCH1_PIN, INPUT_PULLUP);
-  pinMode(SWITCH2_PIN, INPUT_PULLUP);
-  pinMode(SWITCH1_LED_PIN, OUTPUT);
-  pinMode(SWITCH2_LED_PIN, OUTPUT);
-
+  pinMode(SWITCH_PIN, INPUT_PULLUP);
   Serial.println("LoRa init succeeded.");
 }
 
 void loop() {
-  bool newSwitch1State = !digitalRead(SWITCH1_PIN); // Inverted logic
-  bool newSwitch2State = !digitalRead(SWITCH2_PIN); // Inverted logic
+  bool switchState = digitalRead(SWITCH_PIN) == LOW; // Invert the switch state
+  String message = "Switch 1 state changed: " + String(switchState ? "closed" : "open");
 
-  if (newSwitch1State != switch1State) {
-    switch1State = newSwitch1State;
-    sendSwitchState("Switch 1", switch1State);
-    digitalWrite(SWITCH1_LED_PIN, switch1State ? HIGH : LOW); // Update LED status
-  }
-
-  if (newSwitch2State != switch2State) {
-    switch2State = newSwitch2State;
-    sendSwitchState("Switch 2", switch2State);
-    digitalWrite(SWITCH2_LED_PIN, switch2State ? HIGH : LOW); // Update LED status
-  }
-
-  // Check for incoming LoRa packets
-  int packetSize = LoRa.parsePacket();
-  if (packetSize) {
-    String received = "";
-    while (LoRa.available()) {
-      char c = (char)LoRa.read();
-      received += c;
-    }
-
-    // If a "requestSwitchStates" message is received, send the current switch states
-    if (received == "requestSwitchStates") {
-      sendSwitchState("Switch 1", switch1State);
-      digitalWrite(SWITCH1_LED_PIN, switch1State ? HIGH : LOW); // Update LED status
-      sendSwitchState("Switch 2", switch2State);
-      digitalWrite(SWITCH2_LED_PIN, switch2State ? HIGH : LOW); // Update LED status
-    }
-  }
-}
-
-void sendSwitchState(const char* switchName, bool switchState) {
   LoRa.beginPacket();
-  LoRa.print(switchName);
-  LoRa.print(": ");
-  LoRa.print(switchState ? "closed" : "open");
+  LoRa.print(message);
   LoRa.endPacket();
+
+  Serial.println("Sent message: " + message); // Print the sent message
+
 }
